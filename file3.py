874ef365c9ed9739e4692cf5125ce6dfbbb6b116
Amey-274 <nonhackable1290@gmail.com>
2021-03-16 11:46:01 +0530

    chanage first commit

--- file3.py
+++ file3.py
@@ -11,13 +11,3 @@ def name1():
 def func():
     return "hello World"
 
-def func1():
-    return "hello1"
-
-    #  ssh-keygen -t ed25519 -C "nonhackable190@gmail.com"
-    #  eval $(ssh-agent -s)
-    #  tail ~/.ssh/id_rsa.pub
-# sssss
-
-def func12():
-    return
