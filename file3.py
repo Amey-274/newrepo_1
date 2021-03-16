a27daa27e5238dcddfcf1db909acc0a9a2fdcace
Amey-274 <nonhackable1290@gmail.com>
2021-03-16 11:42:34 +0530

    first commit

---
+++ file3.py
@@ -0,0 +1,23 @@
+def name():
+    print("hello World")
+    return 
+
+
+
+def name1():
+    print("hello")
+
+
+def func():
+    return "hello World"
+
+def func1():
+    return "hello1"
+
+    #  ssh-keygen -t ed25519 -C "nonhackable190@gmail.com"
+    #  eval $(ssh-agent -s)
+    #  tail ~/.ssh/id_rsa.pub
+# sssss
+
+def func12():
+    return
