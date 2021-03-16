90e8ca50e8111d381c3387f2f4f23e61c2592a1b
Amey-274 <nonhackable1290@gmail.com>
2021-03-16 11:49:31 +0530

    add some data in python file

--- file3.py
+++ file3.py
@@ -11,3 +11,14 @@ def name1():
 def func():
     return "hello World"
 
+def func1():
+    return "hello1"
+
+    #  ssh-keygen -t ed25519 -C "nonhackable190@gmail.com"
+    #  eval $(ssh-agent -s)
+    #  tail ~/.ssh/id_rsa.pub
+# sssss
+
+def func12():
+    return
+
